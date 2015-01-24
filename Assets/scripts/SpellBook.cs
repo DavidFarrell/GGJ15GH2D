2c9cd90727d3bfa12ffa60bce99a059b47bc3f9c
Colin Farrell <hiddeninjaskills@gmail.com>
2015-01-24 23:29:29 +0000

    colinscene with spellbook

--- Assets/scripts/SpellBook.cs
+++ Assets/scripts/SpellBook.cs
@@ -53,29 +53,37 @@ public class SpellBook : MonoBehaviour {
 
 
 		List<string> players = new List<string> (){
-			"Red_Wizard", "Blue_Wizard", "Green_Wizard", "Yellow_Wizard"};
+			"Player1_Widget_SpawnPoint", "Player2_Widget_SpawnPoint", "Player3_Widget_SpawnPoint", "Player4_Widget_SpawnPoint"};
 
 
-		Shuffle (players);
+	//	Shuffle (players);
 
 
 		Player1Spell = (GameObject)Instantiate (leftJoySpell);
 		GameObject player1 = GameObject.Find (players[0]);
 		Player1Spell.transform.parent = player1.transform;
+		Player1Spell.transform.localPosition = new Vector3 (0, 0, 0);
 		Player1Spell.GetComponent<Spell> ().setLeftJoystick (true);
+		Player1Spell.SetActive (true);
 
 		Player2Spell = (GameObject)Instantiate (rightJoySpell);
 		GameObject player2 = GameObject.Find (players[1]);
 		Player2Spell.transform.parent = player2.transform;
+		Player2Spell.transform.localPosition = new Vector3 (0, 0, 0);
 		Player2Spell.GetComponent<Spell> ().setLeftJoystick (false);
+		Player2Spell.SetActive (true);
 
 		Player3Spell = (GameObject)Instantiate (faceSpell);
 		GameObject player3 = GameObject.Find (players[2]);
 		Player3Spell.transform.parent = player3.transform;
+		Player3Spell.transform.localPosition = new Vector3 (0, 0, 0);
+		Player3Spell.SetActive (true);
 
 		Player4Spell = (GameObject)Instantiate (triggerSpell);
 		GameObject player4 = GameObject.Find (players[3]);
 		Player4Spell.transform.parent = player4.transform;
+		Player4Spell.transform.localPosition = new Vector3 (0, 0, 0);
+		Player4Spell.SetActive (true);
 
 
 	}
