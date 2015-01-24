05b13514711c234d754dad0ef573e027dfe91281
Colin Farrell <hiddeninjaskills@gmail.com>
2015-01-24 00:51:36 +0000

    waggle empty script

---
+++ Assets/scripts/Waggle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waggle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (Input.GetAxis ("R_XAxis"));
+		
+
+	}
+
+
+	ß
+
+}
