27e62815e949b2d8df123e6c0ee3e6beae5623ad
Colin Farrell <hiddeninjaskills@gmail.com>
2015-01-24 19:13:31 +0000

    heliprefab - starting spellbook

---
+++ Assets/scripts/SpellBook.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpellBook : MonoBehaviour {
+
+	public GameObject Player1Spell;
+	public GameObject Player2Spell;
+	public GameObject Player3Spell;
+	public GameObject Player4Spell;
+
+	//public GameObject[] spells;
+
+	public GameObject[] joystickSpells;
+	public GameObject[] faceSpells;
+	public GameObject[] triggerSpells;
+
+
+
+	// Use this for initialization
+	void Start () {
+		assignSpells ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void assignSpells(){
+
+
+	}
+
+
+}
