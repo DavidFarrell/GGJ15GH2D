5c0f3df30cf866b9ff651f58267af674ca414013
DavidFarrell <dfarrell@davidlearnsgames.com>
2015-01-24 17:43:12 +0000

    starting on middle button sync

---
+++ Assets/CastSpell.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CastSpell : Spell {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if Input.GetJoystickNames
+	}
+}
