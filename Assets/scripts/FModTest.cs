8bbc185d075698f5776cd94f21c507362e386fc6
DavidFarrell <dfarrell@davidlearnsgames.com>
2015-01-24 11:04:03 +0000

    Integrating Art
    Bunch of art assets over Friday night

--- Assets/scripts/FModTest.cs
+++ Assets/scripts/FModTest.cs
@@ -5,26 +5,31 @@ public class FModTest : MonoBehaviour {
 	FMOD.Studio.EventInstance fmMusic;
 	FMOD.Studio.ParameterInstance fmProgression;
 
+	FMOD.Studio.EventInstance fmSingle;
 
 	// Use this for initialization
 	void Start () {
-		
+
+		fmSingle = FMOD_StudioSystem.instance.GetEvent ("event:/Single Shot");
+	
+
 		//FMOD_StudioSystem.instance.PlayOneShot ("event:/Single Shot", transform.position);
-		fmMusic = FMOD_StudioSystem.instance.GetEvent("event:/Music");
+		/*fmMusic = FMOD_StudioSystem.instance.GetEvent("event:/Music");
 		fmMusic.start();
-		fmMusic.getParameter("Progression", out fmProgression);
+		fmMusic.getParameter("Progression", out fmProgression);*/
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown ("space")) {
 			print ("space key was pressed");
-			fmProgression.setValue(100);
+			fmSingle.start ();
+		//	fmProgression.setValue(100);
 		}
 	}
 
 	void OnDisable()
 	{
-		fmMusic.stop (FMOD.Studio.STOP_MODE.IMMEDIATE);
+		//fmMusic.stop (FMOD.Studio.STOP_MODE.IMMEDIATE);
 	}
 }
