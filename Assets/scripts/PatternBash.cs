167113128853d0f86c371ad1d0bf0f39c02b1c8d
Ross Anderson <admin@LAB-M627-Mac-08.local>
2015-01-24 00:18:06 +0000

    Fixed pattern script
    no errors, but not complete

--- Assets/scripts/PatternBash.cs
+++ Assets/scripts/PatternBash.cs
@@ -5,8 +5,8 @@ using System.Collections;
 public class PatternBash : MonoBehaviour {
 
 	public string pattern; /*Pattern string containing letters A,B,X or Y e.g "ABXY"*/
-	private bool[] oldPresses = new Boolean[4]; /*ABXY, 1 is pressed 0 is depressed*/
-	private bool[] keyPresses = new Boolean[4]; 
+	private bool[] oldPresses = new bool[4]; /*ABXY, 1 is pressed 0 is depressed*/
+	private bool[] keyPresses = new bool[4]; 
 	private int cursor = 0;
 	
 	// Use this for initialization
@@ -17,19 +17,42 @@ public class PatternBash : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		//Reset keypresses and store previous
+		oldPresses = copyBool(keyPresses);
+		keyPresses = new bool[]{false,false,false,false};
+
 		//Poll for input
-		if(Input.getButtonDown("")) {
-			;
+
+		if (Input.GetButtonDown("ButtonA")) {
+			keyPresses[0] = true;
 		}
+		if (Input.GetButtonDown("ButtonB")) {
+			keyPresses[1] = true;
+		}
+		if (Input.GetButtonDown("ButtonX")) {
+			keyPresses[2] = true;
+		}
+		if (Input.GetButtonDown("ButtonY")) {
+			keyPresses[3] = true;
+		}
+		
 	}
 
 	void successPress(){
-		Debug.log("Hit!");
+		Debug.Log("Hit!");
 		//Increase the player's health-bar or something
 	}
 	void failPress() {
-		Debug.log("Fail!");
+		Debug.Log("Fail!");
 		//Reduce the player's health or something
+	}
+
+	bool[] copyBool(bool[] keyPresses) {
+		bool[] presses = new bool[keyPresses.Length];
+		for (int i = 0; i < keyPresses.Length;i++) {
+			presses[i] = keyPresses[i];
+		}
+		return presses;
 	}
 
 }
