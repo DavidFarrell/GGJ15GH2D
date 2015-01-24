0e3fe4ab71a85e4dfbe22059a5cb0f86f4ce3b11
Colin Farrell <hiddeninjaskills@gmail.com>
2015-01-24 05:10:50 +0000

    waggle sprite hookup

--- Assets/scripts/Waggle.cs
+++ Assets/scripts/Waggle.cs
@@ -1,18 +1,21 @@
 ﻿using UnityEngine;
+
 using System.Collections;
 
 public class Waggle : Spell {
 	
 	public string axis = "R_XAxis";
 
+
 	
 	private float joystickInput;
 	private float lastWaggle = 1.0f;
+	private SpriteRenderer[] waggleSprites;
 
 	
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
@@ -25,7 +28,7 @@ public class Waggle : Spell {
 		} else
 			modifyPower (timeDecay);
 		//	Debug.Log (joystickInput);
-		Debug.Log (currentPower());
+
 		
 		
 	}
@@ -39,6 +42,11 @@ public class Waggle : Spell {
 	public bool waggleChanged(){
 		if (joystickInput == -lastWaggle) {
 			lastWaggle = joystickInput;
+			waggleSprites = GetComponentsInChildren<SpriteRenderer>();
+			foreach (SpriteRenderer sprite in waggleSprites)
+			{
+				sprite.enabled = !sprite.enabled;
+			}
 			return true;
 		}
 		return false;
