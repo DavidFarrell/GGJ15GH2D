8c12f90e3b39678e242c7655f3ef5555b3bbe09b
DavidFarrell <dfarrell@davidlearnsgames.com>
2015-01-25 00:50:40 +0000

    Added a basic timer

--- Assets/scripts/CountdownTimer.cs
+++ Assets/scripts/CountdownTimer.cs
@@ -7,7 +7,6 @@ public class CountdownTimer : MonoBehaviour {
 	private int power;
 	public float timeLeft = 30f;
 	public float initialTime = 30f;
-	public float speedFactor = 1f;
 
 	// Use this for initialization
 	protected void Start () {
@@ -20,7 +19,7 @@ public class CountdownTimer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		timeLeft -= (Time.deltaTime)*speedFactor;
+		timeLeft -= (Time.deltaTime);
 		float percentage = timeLeft / initialTime;
 		myPowerBar.currentPower = percentage*100;
 
