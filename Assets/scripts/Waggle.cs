ef944b5dd105c6e13b6ccfd0d128601f729b5b29
Colin Farrell <hiddeninjaskills@gmail.com>
2015-01-25 01:07:59 +0000

    waggle penalty check started

--- Assets/scripts/Waggle.cs
+++ Assets/scripts/Waggle.cs
@@ -7,9 +7,11 @@ using System.Collections;
 public class Waggle : Spell {
 	
 	public string axis = "R_XAxis";
+	private string penaltyAxis;
 	private bool isLR = true;
 	public bool isLeftStick;
 	private float joystickInput;
+	private float penaltyInput;
 	private float lastWaggle = 1.0f;
 
 	
@@ -33,7 +35,15 @@ public class Waggle : Spell {
 		joystickInput = Input.GetAxis (axis);
 		if (joystickInput > 0.8f) joystickInput = 1;
 		if (joystickInput < -0.8f) joystickInput = -1;
+
+		checkForPenalty ();
 	}
+
+	void checkForPenalty(){
+		penaltyInput = Input.GetAxis (penaltyAxis);
+		if (penaltyInput > 0.8f) penaltyInput = 1;
+		if (penaltyInput < -0.8f) penaltyInput = -1;
+		}
 	
 	public bool waggleChanged(){
 		if (joystickInput == -lastWaggle) {
@@ -47,6 +57,7 @@ public class Waggle : Spell {
 	public override void setLeftJoystick(bool isLeft) {
 		isLeftStick = isLeft;
 		axis = isLeftStick ?  "L_XAxis" : "R_XAxis";
+		penaltyAxis = isLeftStick ?  "L_YAxis" : "R_YAxis";
 		GameObject stickText = transform.FindChild("PowerBar/StickID").gameObject;
 		stickText.GetComponent<Text>().text = isLeft ? "LS" : "RS" ;
 	}
